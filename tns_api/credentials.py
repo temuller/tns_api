e22f396b85d8872de7e85d696bab751e29200e67
temuller <t.e.muller-bravo@tcd.ie>
2025-04-23 11:34:03 +0100

    accepting input and hiding output file

--- tns_api/credentials.py
+++ tns_api/credentials.py
@@ -6,19 +6,25 @@ from dotenv import load_dotenv
 import tns_api
 api_path = tns_api.__path__[0]
 
-def set_credentials():
-    print("🔐 Please enter your TNS credentials:")
-    
-    tns_id = input("TNS ID: ")
-    tns_bot_name = input("TNS BOT NAME: ")
-    tns_api_key = input("TNS API KEY: ")
-    cred_dict = {"tns_id": tns_id,
-                 "tns_bot_name": tns_bot_name,
-                 "tns_api_key": tns_api_key,
-                 }
-    credentials_file = Path(api_path, "credentials.yml")
+def set_credentials(credentials: dict = None) -> None:
+    """Set the TNS credentials for the API.
+
+    Parameters
+    ----------
+    credentials: dictionary with "tns_id", "tns_bot_name" and "tns_api_key".
+    """
+    if credentials is None:
+        print("🔐 Please enter your TNS credentials:")
+        tns_id = input("TNS ID: ")
+        tns_bot_name = input("TNS BOT NAME: ")
+        tns_api_key = input("TNS API KEY: ")
+        cred_dict = {"tns_id": tns_id,
+                     "tns_bot_name": tns_bot_name,
+                     "tns_api_key": tns_api_key,
+                     }
+    credentials_file = Path(api_path, ".credentials.yml")
     with open(credentials_file, 'w') as outfile:
-        yaml.dump(cred_dict, outfile, default_flow_style=False)
+        yaml.dump(credentials, outfile, default_flow_style=False)
     print("TNS credentials stored successfully!")
     print("Please, restart your kernel to make the changes effective.")
 
@@ -33,7 +39,7 @@ def load_yml(file: str) -> dict:
 
 # load credentials
 try:
-    credentials_file = Path(api_path, "credentials.yml")
+    credentials_file = Path(api_path, ".credentials.yml")
     credentials = load_yml(credentials_file)
 
     TNS_ID = credentials["tns_id"]
